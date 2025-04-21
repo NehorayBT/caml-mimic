f897a651aca1540bc97ea69aba3f1dabc419b411
NehorayBT <nehoray@mail.tau.ac.il>
2025-04-21 09:40:10 +0300

    changed README

--- constants.py
+++ constants.py
@@ -2,11 +2,10 @@ PAD_CHAR = "**PAD**"
 EMBEDDING_SIZE = 100
 MAX_LENGTH = 2500
 
-REPO_DIR = 'D:/Nehoray/AB University/NLP/Project/CAML env 2/caml-mimic'
+REPO_DIR = ''
 
 #where you want to save any models you may train
 MODEL_DIR = f'{REPO_DIR}/saved_models/'
 
 DATA_DIR = f'{REPO_DIR}/mimicdata/'
-MIMIC_4_DIR = f'{REPO_DIR}/mimicdata/mimic3'
-MIMIC_2_DIR = f'{REPO_DIR}/mimicdata/mimic2'
+MIMIC_4_DIR = f'{REPO_DIR}/mimicdata/mimic4'
